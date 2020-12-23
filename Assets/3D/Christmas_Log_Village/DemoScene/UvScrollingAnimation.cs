b3ccde8641b1a60d5e11e4e0af1607b9fc04bfe0
Roman <LuxuryPanda@users.noreply.github.com>
2020-12-23 17:12:32 +0100

    Revert "[UPDATE] Sistemate le animazioni di Babbo Natale, aggiunta la scritta di auguri e fatta la prima build"
    This reverts commit 36f93895b05dbb5c73020757e84fa962b2c9b043.

--- Assets/3D/Christmas_Log_Village/DemoScene/UvScrollingAnimation.cs
+++ Assets/3D/Christmas_Log_Village/DemoScene/UvScrollingAnimation.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
+using System.Collections;
 
-public class UvScrollingAnimation : MonoBehaviour
-{
+public class UvScrollingAnimation : MonoBehaviour {
+
+
 	public Vector2 UvSpeed;
+
+	void Start () {
 	
-	void Update()
-	{
+	}
+	
+
+	void Update () {
+
 		this.GetComponent<Renderer>().material.mainTextureOffset += UvSpeed;
+	
 	}
 }
